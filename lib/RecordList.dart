b51c71bd88be510cf5c09fc91886759d14fa30a7
syxxne <tjdus562@naver.com>
2022-12-01 14:10:19 +0900

    import keyboard size library

---
+++ lib/RecordList.dart
@@ -0,0 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class RecordList extends StatefulWidget {
+  const RecordList({Key? key}) : super(key: key);
+
+  @override
+  State<RecordList> createState() => _RecordListState();
+}
+
+class _RecordListState extends State<RecordList> {
+  bool isLoading = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            '일기 리스트',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // TODO
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('records')
+              .where('user', isEqualTo: 'user1@example.com') // TODO
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final docs = snapshot.data!.docs;
+            return GridView.builder(
+              itemCount: docs.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1,
+              ),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GridTile(
+                      child: Card(
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: InkWell(
+                      onTap: () {
+                        print(docs[index].id); // TODO
+                      },
+                      child: Stack(
+                        alignment: Alignment.bottomLeft,
+                        children: [
+                          Image.network(
+                            'https://picsum.photos/200', // TODO
+                            width: 200,
+                            height: 200,
+                          ),
+                          Container(
+                            color: Colors.white,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(docs[index]['date']),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+                );
+              },
+            );
+          },
+        ));
+  }
+
+  Widget showRecordList() {
+    if (isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    } else {
+      setState(() {
+        // getRecordList();
+        isLoading = false;
+      });
+      return const Text('done');
+    }
+  }
+
+  void getRecordList() async {
+    QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('records').get();
+    print(snapshot.docs);
+    isLoading = false;
+  }
+}
---
+++ lib/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor extends Color {
+  AppColor(super.value);
+
+  static const bluePruple = Color(0xff4055f2);
+}
